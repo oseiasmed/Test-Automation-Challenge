72d8bc8ca3075ecb96443837bc7440b979ee10db
oseiasmed <oseias.medeiros@lawsoft.com.br>
2022-04-09 20:27:10 -0300

    Reconfigurando o arquivo dotenv

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
-export const username = 'oseias.medeiros@lawsoft.com.br'
-export const password = 'Analyst_TOP@2022'
+require('dotenv').config()
+
+export const username = process.env.USER
+export const password = process.env.PASSWORD
 export const timeout = 500000
+
+// export const username = 'oseias.medeiros@lawsoft.com.br'
+// export const password = 'Analyst_TOP@2022'
+// export const timeout = 500000
