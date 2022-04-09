13609509463b8cd9f881748513d14ad499ec7ee8
oseiasmed <oseias.medeiros@lawsoft.com.br>
2022-04-09 20:32:12 -0300

    Configurando o dotenv example

--- config.js
+++ config.js
@@ -4,6 +4,3 @@ export const username = process.env.USER
 export const password = process.env.PASSWORD
 export const timeout = 500000
 
-// export const username = 'oseias.medeiros@lawsoft.com.br'
-// export const password = 'Analyst_TOP@2022'
-// export const timeout = 500000
